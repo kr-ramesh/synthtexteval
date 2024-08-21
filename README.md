c9d0c5b54166d70acb0b02462959eaf8a4e941d1
Krithika Ramesh <kramesh3@smtp.johnshopkins.edu>
2024-08-21 11:50:24 -0400

    Adding some baseline data files to experiment with. Note: Don't make this repo public, ccreate a new repo in case code needs to be shared, as the data files can't be made publically available.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sdg-medical
+version https://git-lfs.github.com/spec/v1
+oid sha256:c93fd1e020e7028eb279442d1a9afe216cfea074a6a1a723e4aedd7ff71bbace
+size 13
--- downstream/classification/sample_script.sh
+++ downstream/classification/sample_script.sh
@@ -1,27 +1,3 @@
-#!/bin/bash
-
-#SBATCH --job-name="random_mul_icd_2"
-#SBATCH --time=48:00:00
-#SBATCH --account=a100acct
-#SBATCH --gres=gpu:1
-#SBATCH --partition=gpu-a100
-#SBATCH --output=inf-ops/reranked_full.txt
-
-#Sample script for training models
-foldername="princeton_mimic_10ICD_DP_8"
-python icd_classification.py \
-            --dataset_name "mimic" \
-            --path_to_dataset "data-updated/$foldername/" \
-            --is_train \
-            --path_to_model "models-test/$foldername/" \
-            --n_labels 10
-
-#Sample script for testing models
-baseline_path="baseline_downstream_data_10ICD"
-python icd_classification.py 
-            --dataset_name "mimic" \
-            --path_to_dataset "data/$baseline_path/" \
-            --is_test \
-            --path_to_model "models/princeton_mimic_10ICD_DP_8" \
-            --csv_output_path "outputs-from-testing.csv" \
-            --n_labels 10
+version https://git-lfs.github.com/spec/v1
+oid sha256:49d3d0abedc0325a6893fbb3747d84de8a67492e46369e8a066eab61dc730fb8
+size 885
