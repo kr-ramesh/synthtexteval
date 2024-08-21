fb055dcceed08061a67e1d7a02a9423bed90e457
Krithika Ramesh <kramesh3@smtp.johnshopkins.edu>
2024-08-21 11:47:17 -0400

    Adding files to train and test models for the downstream multilabelling task with MIMIC-III ICD9 codes.

---
+++ downstream/classification/sample_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name="random_mul_icd_2"
+#SBATCH --time=48:00:00
+#SBATCH --account=a100acct
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu-a100
+#SBATCH --output=inf-ops/reranked_full.txt
+
+#Sample script for training models
+foldername="princeton_mimic_10ICD_DP_8"
+python icd_classification.py \
+            --dataset_name "mimic" \
+            --path_to_dataset "data-updated/$foldername/" \
+            --is_train \
+            --path_to_model "models-test/$foldername/" \
+            --n_labels 10
+
+#Sample script for testing models
+baseline_path="baseline_downstream_data_10ICD"
+python icd_classification.py 
+            --dataset_name "mimic" \
+            --path_to_dataset "data/$baseline_path/" \
+            --is_test \
+            --path_to_model "models/princeton_mimic_10ICD_DP_8" \
+            --csv_output_path "outputs-from-testing.csv" \
+            --n_labels 10
