0fba5a619b16abd6cfadbe81663a9bd8b31e1015
Krithika Ramesh <43345660+kr-ramesh@users.noreply.github.com>
2024-12-05 18:05:41 -0500

    Delete dist-diff directory

--- dist-diff/diff.sh
+++
@@ -1,5 +0,0 @@
-ref_path="/home/kramesh3/pyjak/data-final/princeton_mimic_10ICD_DP_8.csv"
-base_path="/home/kramesh3/pyjak/data-final/princeton_mimic_10ICD_DP_inf.csv"
-
-python analyze.py --ref_data_path $ref_path \
-                  --base_data_path $base_path \
