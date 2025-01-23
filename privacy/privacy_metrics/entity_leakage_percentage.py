3818b5d0ecfcc88cfeb8f70f7e69c297a8a73cfa
zzhao71 <zzhao71@jh.edu>
2025-01-22 23:35:47 -0500

    update

--- privacy/privacy_metrics/entity_leakage_percentage.py
+++ privacy/privacy_metrics/entity_leakage_percentage.py
@@ -7,12 +7,8 @@ def entities_in_paragraph(paragraph: str, entities: list) -> dict:
 
 if __name__ == "__main__":
     paragraph_text = ""
-   
-    
     entities_to_check = ""
     
     presence_dict = entities_in_paragraph(paragraph_text, entities_to_check)
-    
-    # Print out the result for each entity
     for entity, found in presence_dict.items():
         print(f"Entity '{entity}' found? {found}")
