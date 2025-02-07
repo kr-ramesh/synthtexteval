266f2d5e842d9db4bb204566e61e630f9bb4cf7f
Krithika Ramesh <43345660+kr-ramesh@users.noreply.github.com>
2025-02-07 18:36:50 -0500

    Delete fairness directory

--- fairness/fair.sh
+++
@@ -1,6 +0,0 @@
-#TODO: Format the fairness evaluation, accept arguments and run the script
-
-python eval.py \
-    --path_to_output_predictions ${path_to_output_predictions} \
-    --problem_type ${problem_type} \
-    --num_labels ${n_labels}
